896577ad446a281169a3b4e749b61949368f492b
FanysSs <estefania.rguezb21@gmail.com>
2024-11-26 10:46:35 -0600

    update componente Actividad

---
+++ src/app/actividad/actividad.component.spec.ts
@@ -0,0 +1,23 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActividadComponent } from './actividad.component';
+
+describe('ActividadComponent', () => {
+  let component: ActividadComponent;
+  let fixture: ComponentFixture<ActividadComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ActividadComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActividadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
